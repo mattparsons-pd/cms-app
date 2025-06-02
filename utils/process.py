3a5f754eef6e13b6ae300d2b6ae2247df5e96991
Matt Parsons <mattparsons14@yahoo.com>
2025-06-01 21:58:57 -0500

    modification to last_process

--- utils/process.py
+++ utils/process.py
@@ -3,7 +3,7 @@ import csv
 import json
 import logging
 import requests
-from datetime import datetime,timezone
+from datetime import datetime,timezone,timedelta
 from concurrent.futures import ThreadPoolExecutor
 
 from .transform import snake_case
@@ -14,8 +14,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Process:
-    ATTR_META_FILENAME = "last_run_metadata.json"
-
+    ATTR_META_FILENAME = "metadata.json"
     def __init__(self, url: str, theme: str, output_dir: str, max_workers: int):
         self.url = url
         self.theme = theme
@@ -23,25 +22,45 @@ class Process:
         self.max_workers = max_workers
 
     @property
-    def last_run_time(self):
-        if not os.path.exists(self.ATTR_META_FILENAME):
-            return None
-        with open(self.ATTR_META_FILENAME, "r") as f:
-            return datetime.fromisoformat(json.load(f).get("last_run"))
-    
-    @property   
-    def processed_files(self) -> list[dict]:
-        if not os.path.exists(self.ATTR_META_FILENAME):
-            return None
-        with open(self.ATTR_META_FILENAME, "r") as f:
-            return json.load(f).get("files_processed")
+    def TODAY(self) -> str:
+        return datetime.now().strftime("%Y-%m-%d")
 
-    def save_last_run_time(self, processed_files: list[dict]):
+    @property
+    def METADATA_PATH(self) -> str:
+        return os.path.join(self.output_dir, self.TODAY, "runs")
+    
+    @property
+    def DATA_PATH(self) -> str:
+        return os.path.join(self.output_dir, self.TODAY , "data")
+
+    @property
+    def LAST_RUN_DATE(self):
+        # Returns yesterday's date
+        return str((datetime.now() - timedelta(days=1)).date())
+
+
+    def get_processed_files(self,date_str:str) -> list[dict] | None:
+        input_path = os.path.join(self.output_dir,date_str,"runs",self.ATTR_META_FILENAME)
+        
+        if not os.path.exists(input_path):
+            logger.warning(f"path for {date_str} does not exist. Using {self.METADATA_PATH} instead.")
+            input_path = os.path.join(self.METADATA_PATH,self.ATTR_META_FILENAME)
+            
+        metadata_dict = self.read_path(input_path)
+        return metadata_dict.get("files_meta_data")
+    
+    def read_path(self,input_path:str) -> dict:
+        with open(input_path, "r") as f:
+            return json.load(f)
+        
+    def save_run_time(self, processed_files: list[dict]):
         data = {
             "last_run": datetime.now(timezone.utc).isoformat(),
             "files_meta_data": processed_files
         }
-        with open(self.ATTR_META_FILENAME, "w") as f:
+        output_path = os.path.join(self.METADATA_PATH, self.ATTR_META_FILENAME)
+        os.makedirs(self.METADATA_PATH, exist_ok=True)
+        with open(output_path, "w") as f:
             json.dump(data, f, indent=2)
 
     def fetch_metadata_theme(self) -> list[dict]:
@@ -50,7 +69,9 @@ class Process:
         response.raise_for_status()
         
         filtered = [
-            {"modified": item["modified"],"file_location":item["distribution"]}
+            {"id": item["identifier"],
+             "modified": item["modified"],
+             "file_location":item["distribution"]}
             for item in response.json()
             if self.theme in item.get("theme", [])
         ]
@@ -59,19 +80,23 @@ class Process:
             logger.warning(f"No datasets found with theme '{self.theme}'.")
 
         return filtered
-
+    
+    def last_processed(self, id: str) -> str | None:
+        processed_files = self.get_processed_files(self.LAST_RUN_DATE)
+        for file in processed_files:
+            if id in file["id"]:
+                last_processed_date = file["modified"]
+        return last_processed_date or None
+        
     def is_modified_since(self, item: dict) -> bool:
         modified = item.get("modified")
+        id = item.get("id")
+        #account for first time processing runs
         if not modified:
             return False
 
-        # Parse as offset-aware UTC datetime
-        mod_time = datetime.fromisoformat(modified)
-        if mod_time.tzinfo is None:
-            mod_time = mod_time.replace(tzinfo=timezone.utc)
-
-        last_run = self.last_run_time
-        return not last_run or mod_time > last_run
+        last_run = self.last_processed(id)
+        return modified > last_run
 
     def get_csv_distributions(self, item: dict) -> list[str]:
         return [
@@ -92,9 +117,9 @@ class Process:
             output_rows = [dict(zip(snake_headers, row.values())) for row in reader]
 
             file_name = os.path.basename(url.split("?")[0])
-            output_path = os.path.join(self.output_dir, file_name)
+            output_path = os.path.join(self.DATA_PATH, file_name)
 
-            os.makedirs(self.output_dir, exist_ok=True)
+            os.makedirs(self.DATA_PATH, exist_ok=True)
             with open(output_path, "w", newline="") as f:
                 writer = csv.DictWriter(f, fieldnames=snake_headers)
                 writer.writeheader()
@@ -107,9 +132,9 @@ class Process:
     def run(self) -> None:
         logger.info("Starting processing run.")
         metadata = self.fetch_metadata_theme()
-        last_run = self.last_run_time
+        last_run = os.path.exists(self.METADATA_PATH)
 
-        if last_run is None:
+        if not last_run:
             logger.info("First-time run: processing all datasets matching theme.")
             modified_datasets = metadata
         else:
@@ -127,5 +152,5 @@ class Process:
         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             executor.map(self.download_and_process_csv, urls)
 
-        self.save_last_run_time(processed_files=metadata)
+        self.save_run_time(processed_files=metadata)
         logger.info("Job completed.")
